7a956e6db743f5839df69509be856799a94bdb42
subbaSahil <sahil_s@pursuitsoftware.com>
2025-02-26 12:33:12 +0530

    ne

--- src/main/java/Pages/RoleSelectionPage.java
+++ src/main/java/Pages/RoleSelectionPage.java
@@ -28,7 +28,7 @@ public class RoleSelectionPage {
     public void signIn() {
     	this.interactions.enterText(this.commonlocators.microsoftEmailField, "sahil_s@pursuitsoftware.com");
     	this.interactions.click(this.commonlocators.nextButton);
-    	this.interactions.enterText(this.commonlocators.microsoftPasswordField, this.loginPassword);
+    	this.interactions.enterText(this.commonlocators.microsoftPasswordField, "Pursuit8257988069");
     	this.interactions.click(this.commonlocators.nextButton);
     	System.out.println(this.interactions.getText(this.commonlocators.authText));
     	this.interactions.click(this.commonlocators.nextButton);
